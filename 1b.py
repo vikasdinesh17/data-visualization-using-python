55b3ec252a9eec3bfb565af5189aeee20c6742b2
VIKAS D <122000473+vikasdinesh17@users.noreply.github.com>
2024-01-24 15:35:18 +0530

    Create 1b.py

---
+++ 1b.py
@@ -0,0 +1,20 @@
+num = int(input("Enter number:"))
+temp=num
+rev=0
+while(num>0):
+  dig = num % 10
+  rev = rev*10 + dig
+  num = num//10
+if(temp==rev):
+  print(f"The number {temp} is a palindrome!")
+else:
+  print(f"The number {temp} isn't a palindrome!")
+digit_counts = {}
+for digit in str(temp):
+  if digit in digit_counts:
+    digit_counts[digit] += 1
+  else:
+    digit_counts[digit] = 1
+print("Digit counts in", temp, ":")
+for digit, count in digit_counts.items():
+  print(digit, ":", count)
